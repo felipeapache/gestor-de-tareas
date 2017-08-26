cf1120cd297bc58852e567baf90b11f6f2a8e19e
KF <apache142009@hotmail.com>
2017-08-26 16:01:07 -0500

    explicar que es get y post

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 var express = require("express");
 var bodyParser = require("body-parser");
 var server = express();
@@ -7,47 +6,51 @@ server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 
 //http://localhost:3000/login?param=12
-server.get("/login", function(req, res){
-	var parametro2 = parseInt(req.query.param2);
-	var parametro = parseInt(req.query.param);
-	//parseInt convierte de string a numero
-	//req.query devuelve los parametros
-	var hola = sumatoria(parametro);
-	res.json(parametro2 + hola);
+server.get("/login-get", function(req, res){
+	var parametros = req.query;
+	if(req.query.param != undefined){
+		var user = parametros.user;
+		var password = parametros.password;
+		res.json(parametros);
+	} else {
+		res.json("paseme el param");	
+	}	
 });
+
+server.post("/login", function(req, res){
+	var parametros = req.body;
+	var user = validPassword(parametros.password);
+	if(validUser(parametros.user) && user){
+		res.json(user);
+	} else {
+		res.json({ state: "ERROR", description: "El usuario no existe" });	
+	}	
+});
+
+function validPassword(password){
+	for (var i = 0; i < db.length; i++) {
+		if(db[i].password == password){
+			return db[i];
+		}
+	}
+	return false;
+}
+
+
+function validUser(user){
+	for (var i = 0; i < db.length; i++) {
+		if(db[i].user == user){
+			return true;
+		}
+	}
+	return false;
+}
+
+var db = [
+	{ id: 1, name: "Apache", user: "a.pache", password: 123 },
+	{ id: 2, name: "Yeison", user: "y.eison", password: 1234 }
+];
 
 server.listen(3000, function(){
 	console.log("servidor corriendo")
 });
-
-/*function jaja(parametro,holaa){
-	for (var i = 5; i >= 0; i--) {
-		console.log(parametro+holaa)
-	}
-}*/
-
-//jaja("holaa","como estas");
-
-/*function suma(suma,resta) {
-	var total = 0;
-	for (var i = 1; i <= 3; i++) {
-		if(i <= 2){
-			total = i + suma + resta;
-			console.log(total);
-		}	
-	}
-}*/
-//suma(5,12)
-
-function sumatoria(valor){
-    if (valor<=5 ){
-    	var total = 0; 
-		for (var i = 0; i < 3; i++) {
-			total += valor;
-		}	
-		return total;
-	}else{
-		return valor;
-	}
-		
-}
